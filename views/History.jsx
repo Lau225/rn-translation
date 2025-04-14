850650023215e69a9e9d886cee852430702a5999
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2025-04-14 14:52:13 +0800

    first commit

---
+++ views/History.jsx
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function History(){
+    return (
+        <View style={styles.container}>
+            <Text>Hello History</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
