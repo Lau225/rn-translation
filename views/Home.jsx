9e9f909cb49997bc88656a96028992a3ccf78999
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2025-04-17 09:04:00 +0800

    test

--- views/Home.jsx
+++ views/Home.jsx
@@ -23,7 +23,7 @@ export default function HomeScreen() {
                     }))
                 })
                 .catch(err => {
-                    console.log(err)
+                    
                 })
         }
     }
